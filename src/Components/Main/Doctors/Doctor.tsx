2616d681b7b822c94317349b461d5518bef69c2a
Your Name <yaseerbajeh@gmail.com>
2025-09-14 21:43:08 +0300

    bug fixed

--- src/Components/Main/Doctors/Doctor.tsx
+++ src/Components/Main/Doctors/Doctor.tsx
@@ -1,8 +1,6 @@
 type Props = {
   doc: {
     img: string;
-    name: string;
-    job: string;
   };
 };
 
@@ -10,11 +8,7 @@ const Doctor = ({ doc }: Props) => {
   return (
     <div>
       <div className="bg-[#9ae7ff] p-3 pb-0 rounded-tl-[100px] rounded-br-[100px] lg:rounded-tl-[130px] lg:rounded-br-[120px] overflow-hidden">
-        <img className="min-w-[180px]" src={doc.img} alt={doc.name} />
-      </div>
-      <div className="pb-5 pt-3">
-        <h5 className="font-bold">{doc.name}</h5>
-        <p className="text-sm">{doc.job}</p>
+        <img className="min-w-[180px]" src={doc.img} alt="doctor" />
       </div>
     </div>
   );
